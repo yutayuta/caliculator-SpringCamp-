cd3c17768e3885af8d9f108921c4f1f303daf973
yutayuta <arkyuta1013@gmail.com>
2015-03-18 02:27:50 +0900

    ok

--- caliculator spring/ViewController.swift
+++ caliculator spring/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var hyoujiLabel: UILabel!
+    var number: Int! = 0
+    var number2: Int! = 0
+    var operation: Int! = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func select3() {
+        number = number*10 + 3
+        hyoujiLabel.text = String(number)
+    }
+    
+    @IBAction func select4() {
+        number = number*10 + 4
+        hyoujiLabel.text = String(number)
+    }
+    
+    @IBAction func plus(){
+        hyoujiLabel.text = String(0)
+        operation = 1
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func equall() {
+        if operation == 1{
+            hyoujiLabel.text = String(number + number2)
+        }
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
